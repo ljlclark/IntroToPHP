27d9bb440a24923fbcf2d361ce4c9f3e34501055
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-10-09 07:27:54 +0800

    Update Pages

--- PHPCode/LJCTextLib.php
+++ PHPCode/LJCTextLib.php
@@ -11,6 +11,34 @@
       return $retValue;
     }
 
+    // Writes the HTML beginning lines.
+    public static function HTMLBegin(string $outputFileName, string $pageTitle)
+    {
+      self::Line("<!DOCTYPE html>");
+      self::Line("<!-- $outputFileName -->");
+      self::Line("<html lang=\"en\">");
+      self::Line("<head>");
+      self::Line("  <title>$pageTitle</title>");
+      self::Line("  <meta name=\"author\" content=\"Lester J. Clark\" />");
+      self::Line("  <meta charset=\"utf-8\" />");
+      echo("  <meta name=\"viewport\" content=\"width=device-width");
+      self::Line(" initial-scale=1\" />");
+    }
+
+    // Writes the "head" end and "body" begin elements.
+    public static function HTMLBodyBegin()
+    {
+      self::Line("</head>");
+      self::Line("<body>");
+    }
+
+    // Writes the HTML ending lines.
+    public static function HTMLEnd()
+    {
+      self::Line("</body>");
+      self::Line("</html>");
+    }
+
     // Writes the supplied text to the output stream with the new line values.
     public static function Line(?string $text = null, ?bool $display = false
       , ?bool $lastPre = false)
