69dfbb58577ba3c94a2515f2d605ff73e744f9b5
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-10-18 04:53:07 +0800

    Update Pages

--- PHPCode/1TestStrict.php
+++ PHPCode/1TestStrict.php
@@ -3,6 +3,10 @@
   declare(strict_types=1);
 
   $text = "Some text.";
+  echo(gettype($text) . "<br />");
   $text = 5;  // This will display an error.
-  echo($text);
+  echo(gettype($text) . "<br />");
+  $number = $text + 2;
+  echo(gettype($number) . "<br />");
+  echo($number);
 ?>
