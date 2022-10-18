cdd51fe81dfb9ef1e6aed1a3d798f831afe61b0e
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-10-19 00:33:24 +0800

    Update Pages

--- PHPCode/1TestStrict.php
+++ PHPCode/1TestStrict.php
@@ -2,11 +2,31 @@
   // PHPFile.php
   declare(strict_types=1);
 
+  // string
   $text = "Some text.";
-  echo(gettype($text) . "<br />");
+  $type = gettype($text);
+  echo("\$text = $text<br />");
+  echo("\$text type = $type<br />");
+  echo("<br />");
+
+  // int
   $text = 5;  // This will display an error.
-  echo(gettype($text) . "<br />");
-  $number = $text + 2;
-  echo(gettype($number) . "<br />");
-  echo($number);
+  $type = gettype($text);
+  echo("\$text = $text<br />");
+  echo("\$text type = $type<br />");
+  echo("<br />");
+
+  // bool(boolean)
+  $text = true;
+  $type = gettype($text);
+  echo("\$text = $text<br />");
+  echo("\$text type = $type<br />");
+  echo("<br />");
+
+  // float(double)
+  $number = $text + 2.0;
+  $type = gettype($number);
+  echo("\$number = \$text + 2.0<br />");
+  echo("\$number = $number<br />");
+  echo("\$number type = $type<br />");
 ?>
